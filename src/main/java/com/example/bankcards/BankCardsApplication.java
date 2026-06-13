15af016eacbb758a25832e0cca6a0cc6aa3bc48c
guliz <guliztulkinova@gmail.com>
2026-06-13 22:39:04 +0500

    swagger config

--- src/main/java/com/example/bankcards/BankCardsApplication.java
+++ src/main/java/com/example/bankcards/BankCardsApplication.java
@@ -17,21 +17,19 @@ public class BankCardsApplication {
     @Bean
     public CommandLineRunner fixUsers(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
-            // 1. Guliza (ADMIN) ni tekshirish va parolini to'g'rilash
+
             userRepository.findByUsername("guliza").ifPresent(user -> {
                 user.setPassword(passwordEncoder.encode("admin123"));
                 user.setRole("ADMIN");
                 userRepository.save(user);
-                System.out.println(">>>>> 'guliza' paroli 'admin123' ga yangilandi! <<<<<");
-            });
 
-            // 2. Guliza_dev (USER) ni tekshirish va parolini to'g'rilash
+            });
             userRepository.findByUsername("guliza_dev").ifPresent(user -> {
                 user.setPassword(passwordEncoder.encode("user123"));
                 user.setRole("USER");
                 userRepository.save(user);
-                System.out.println(">>>>> 'guliza_dev' paroli 'user123' ga yangilandi! <<<<<");
-            });
+
+                        });
         };
     }
 }
